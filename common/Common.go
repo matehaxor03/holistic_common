eec11bf30a3f2e68ac0083476d88315d7c6c1f50
matehaxor03 <matehaxor03>
2022-12-07 09:49:24 +1100

    forgot to check in the file

--- common/Common.go
+++ common/Common.go
@@ -196,8 +196,6 @@ func GetType(object interface{}) string {
 	return fmt.Sprintf("%T", object)
 }
 
-
-
 func Contains(sl []string, name string) bool {
 	for _, value := range sl {
 		if value == name {
