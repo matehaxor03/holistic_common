a358efeca5d9d650854513e2bd61442a057c9ee2
matehaxor03 <matehaxor03>
2022-12-25 19:17:47 +1100

    make it more simple for wait

--- common/BashCommand.go
+++ common/BashCommand.go
@@ -41,33 +41,29 @@ func NewBashCommand() *BashCommand {
 			wg_stdout.Add(1)
 			stdout_scanner := bufio.NewScanner(cmd_stdout_reader)
 			go func() {
-				wg_stdout.Done()
 				for stdout_scanner.Scan() {
-					wg_stdout.Add(1)
 					text := strings.TrimSpace(stdout_scanner.Text())
 					stdout_array = append(stdout_array, text)
 					if stdout_callback != nil {
 						(*stdout_callback)(text)
 					}
-					wg_stdout.Done()
 				}
+				wg_stdout.Done()
 			}()
 
 			var wg_stderr sync.WaitGroup
 			wg_stderr.Add(1)
 			stderr_scanner := bufio.NewScanner(cmd_stderr_reader)
 			go func() {
-				wg_stderr.Done()
 				for stderr_scanner.Scan() {
-					wg_stderr.Add(1)
 					text := strings.TrimSpace(stderr_scanner.Text())
 					temp_error := fmt.Errorf(text)
 					stderr_array = append(stderr_array, fmt.Errorf(text))
 					if stderr_callback != nil {
 						(*stderr_callback)(temp_error)
 					}
-					wg_stderr.Done()
 				}
+				wg_stderr.Done()
 			}()
 
 
@@ -84,13 +80,13 @@ func NewBashCommand() *BashCommand {
 			if command_wait_err != nil {
 				errors = append(errors, command_wait_err)
 			}
+
+			wg_stdout.Wait()
+			wg_stderr.Wait()
 			
 			if len(errors) > 0 {
 				return nil, errors
 			}
-
-			wg_stdout.Wait()
-			wg_stderr.Wait()
 
 			errors = append(errors, stderr_array...)
 
