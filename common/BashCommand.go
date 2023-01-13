20d22d6f63564b75737d95eeee80da1cc232fbc6
matehaxor03 <matehaxor03>
2023-01-13 14:37:08 +1100

    revert to as before

--- common/BashCommand.go
+++ common/BashCommand.go
@@ -5,8 +5,6 @@ import (
 	"os/exec"
 	"sync"
 	"bufio"
-	"strings"
-	"bytes"
 )
 
 type BashCommand struct {
@@ -22,108 +20,78 @@ func NewBashCommand() *BashCommand {
 
 			cmd := exec.Command("bash", "-c", command)
 
-			if stdout_callback != nil && stderr_callback != nil {
-				cmd_stdout_reader, cmd_stdout_reader_error := cmd.StdoutPipe()
-				if cmd_stdout_reader_error != nil {
-					errors = append(errors, cmd_stdout_reader_error)
-				}
+			cmd_stdout_reader, cmd_stdout_reader_error := cmd.StdoutPipe()
+			if cmd_stdout_reader_error != nil {
+				errors = append(errors, cmd_stdout_reader_error)
+			}
 
-				cmd_stderr_reader, cmd_stderr_reader_error := cmd.StderrPipe()
-				if cmd_stderr_reader_error != nil {
-					errors = append(errors, cmd_stderr_reader_error)
-				}
+			cmd_stderr_reader, cmd_stderr_reader_error := cmd.StderrPipe()
+			if cmd_stderr_reader_error != nil {
+				errors = append(errors, cmd_stderr_reader_error)
+			}
 
-				if len(errors) > 0 {
-					return nil, errors
-				}
+			if len(errors) > 0 {
+				return nil, errors
+			}
 
-				const maxCapacity = 10*1024*1024  
-				
-				var wg_stdout sync.WaitGroup
-				wg_stdout.Add(1)
-				stdout_scanner := bufio.NewScanner(cmd_stdout_reader)
-				stdout_scanner_buffer := make([]byte, maxCapacity)
-				stdout_scanner.Buffer(stdout_scanner_buffer, maxCapacity)
-				stdout_scanner.Split(bufio.ScanLines)
-				go func() {
-					for stdout_scanner.Scan() {
-						text := string(stdout_scanner.Text())
-						stdout_array = append(stdout_array, text)
-						if stdout_callback != nil {
-							go (*stdout_callback)(text)
-						}
+			const maxCapacity = 10*1024*1024  
+			
+			var wg_stdout sync.WaitGroup
+			wg_stdout.Add(1)
+			stdout_scanner := bufio.NewScanner(cmd_stdout_reader)
+			stdout_scanner_buffer := make([]byte, maxCapacity)
+			stdout_scanner.Buffer(stdout_scanner_buffer, maxCapacity)
+			stdout_scanner.Split(bufio.ScanLines)
+			go func() {
+				for stdout_scanner.Scan() {
+					text := string(stdout_scanner.Text())
+					stdout_array = append(stdout_array, text)
+					if stdout_callback != nil {
+						go (*stdout_callback)(text)
 					}
-					wg_stdout.Done()
-				}()
+				}
+				wg_stdout.Done()
+			}()
 
-				var wg_stderr sync.WaitGroup
-				wg_stderr.Add(1)
-				stderr_scanner := bufio.NewScanner(cmd_stderr_reader)
-				stderr_scanner_buffer := make([]byte, maxCapacity)
-				stderr_scanner.Buffer(stderr_scanner_buffer, maxCapacity)
-				stderr_scanner.Split(bufio.ScanLines)
-				var errors_found = false
-				go func() {
-					for stderr_scanner.Scan() {
-						errors_found = true
-						text := string(stderr_scanner.Text())
-						temp_error := fmt.Errorf(text)
-						stderr_array = append(stderr_array, fmt.Errorf(text))
-						if stderr_callback != nil {
-							go (*stderr_callback)(temp_error)
-						}
+			var wg_stderr sync.WaitGroup
+			wg_stderr.Add(1)
+			stderr_scanner := bufio.NewScanner(cmd_stderr_reader)
+			stderr_scanner_buffer := make([]byte, maxCapacity)
+			stderr_scanner.Buffer(stderr_scanner_buffer, maxCapacity)
+			stderr_scanner.Split(bufio.ScanLines)
+			var errors_found = false
+			go func() {
+				for stderr_scanner.Scan() {
+					errors_found = true
+					text := string(stderr_scanner.Text())
+					temp_error := fmt.Errorf(text)
+					stderr_array = append(stderr_array, fmt.Errorf(text))
+					if stderr_callback != nil {
+						go (*stderr_callback)(temp_error)
 					}
-					wg_stderr.Done()
-				}()
-
-
-				command_start_err := cmd.Start()
-				if command_start_err != nil {
-					errors = append(errors, command_start_err)
 				}
-				
-				if len(errors) > 0 {
-					return nil, errors
-				}
+				wg_stderr.Done()
+			}()
 
-				command_wait_err := cmd.Wait()
-				if command_wait_err != nil {
-					errors = append(errors, command_wait_err)
-				}
 
-				wg_stdout.Wait()
-				if errors_found {
-					wg_stderr.Wait()
-					errors = append(errors, stderr_array...)
-				}
-			} else {
+			command_start_err := cmd.Start()
+			if command_start_err != nil {
+				errors = append(errors, command_start_err)
+			}
+			
+			if len(errors) > 0 {
+				return nil, errors
+			}
 
-				var stdout_bytes bytes.Buffer
-				cmd.Stdout = &stdout_bytes
-				var error_bytes bytes.Buffer
-				cmd.Stderr = &error_bytes
-		
-				command_errors := cmd.Run()
-				if command_errors != nil {
-					errors = append(errors, command_errors)
-				}
+			command_wait_err := cmd.Wait()
+			if command_wait_err != nil {
+				errors = append(errors, command_wait_err)
+			}
 
-				if len(errors) > 0 {
-					return nil, errors
-				}
-
-				fmt.Println(stdout_bytes.String())
-				fmt.Println(error_bytes.String())
-
-				string_stdout_lines := strings.Split(stdout_bytes.String(), "\n")
-				for _, string_stdout_line := range string_stdout_lines {
-					stdout_array = append(stdout_array, string_stdout_line)
-				}
-
-				string_stderr_lines := strings.Split(error_bytes.String(), "\n")
-				for _, string_stderr_line := range string_stderr_lines {
-					errors = append(errors, fmt.Errorf("%s", string_stderr_line))
-				}
+			wg_stdout.Wait()
+			if errors_found {
+				wg_stderr.Wait()
+				errors = append(errors, stderr_array...)
 			}
 
 			if len(errors) > 0 {
