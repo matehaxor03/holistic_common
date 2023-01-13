06f09c37b67dcc8d06ec579b33c76a93c58bb192
matehaxor03 <matehaxor03>
2023-01-13 22:50:06 +1100

    do not use stdout and stderror if no callbacks used

--- common/BashCommand.go
+++ common/BashCommand.go
@@ -20,59 +20,61 @@ func NewBashCommand() *BashCommand {
 
 			cmd := exec.Command("bash", "-c", command)
 
-			cmd_stdout_reader, cmd_stdout_reader_error := cmd.StdoutPipe()
-			if cmd_stdout_reader_error != nil {
-				errors = append(errors, cmd_stdout_reader_error)
-			}
-
-			cmd_stderr_reader, cmd_stderr_reader_error := cmd.StderrPipe()
-			if cmd_stderr_reader_error != nil {
-				errors = append(errors, cmd_stderr_reader_error)
-			}
-
-			if len(errors) > 0 {
-				return nil, errors
-			}
-
-			const maxCapacity = 10*1024*1024  
-			
-			var wg_stdout sync.WaitGroup
-			wg_stdout.Add(1)
-			stdout_scanner := bufio.NewScanner(cmd_stdout_reader)
-			stdout_scanner_buffer := make([]byte, maxCapacity)
-			stdout_scanner.Buffer(stdout_scanner_buffer, maxCapacity)
-			stdout_scanner.Split(bufio.ScanLines)
-			go func() {
-				for stdout_scanner.Scan() {
-					text := string(stdout_scanner.Text())
-					stdout_array = append(stdout_array, text)
-					if stdout_callback != nil {
-						go (*stdout_callback)(text)
-					}
-				}
-				wg_stdout.Done()
-			}()
-
-			var wg_stderr sync.WaitGroup
-			wg_stderr.Add(1)
-			stderr_scanner := bufio.NewScanner(cmd_stderr_reader)
-			stderr_scanner_buffer := make([]byte, maxCapacity)
-			stderr_scanner.Buffer(stderr_scanner_buffer, maxCapacity)
-			stderr_scanner.Split(bufio.ScanLines)
 			var errors_found = false
-			go func() {
-				for stderr_scanner.Scan() {
-					errors_found = true
-					text := string(stderr_scanner.Text())
-					temp_error := fmt.Errorf(text)
-					stderr_array = append(stderr_array, fmt.Errorf(text))
-					if stderr_callback != nil {
-						go (*stderr_callback)(temp_error)
-					}
-				}
-				wg_stderr.Done()
-			}()
+			var wg_stdout sync.WaitGroup
+			var wg_stderr sync.WaitGroup
 
+			if stdout_callback != nil && stderr_callback != nil {
+				cmd_stdout_reader, cmd_stdout_reader_error := cmd.StdoutPipe()
+				if cmd_stdout_reader_error != nil {
+					errors = append(errors, cmd_stdout_reader_error)
+				}
+
+				cmd_stderr_reader, cmd_stderr_reader_error := cmd.StderrPipe()
+				if cmd_stderr_reader_error != nil {
+					errors = append(errors, cmd_stderr_reader_error)
+				}
+
+				if len(errors) > 0 {
+					return nil, errors
+				}
+
+				const maxCapacity = 10*1024*1024  
+				
+				wg_stdout.Add(1)
+				stdout_scanner := bufio.NewScanner(cmd_stdout_reader)
+				stdout_scanner_buffer := make([]byte, maxCapacity)
+				stdout_scanner.Buffer(stdout_scanner_buffer, maxCapacity)
+				stdout_scanner.Split(bufio.ScanLines)
+				go func() {
+					for stdout_scanner.Scan() {
+						text := string(stdout_scanner.Text())
+						stdout_array = append(stdout_array, text)
+						if stdout_callback != nil {
+							go (*stdout_callback)(text)
+						}
+					}
+					wg_stdout.Done()
+				}()
+
+				wg_stderr.Add(1)
+				stderr_scanner := bufio.NewScanner(cmd_stderr_reader)
+				stderr_scanner_buffer := make([]byte, maxCapacity)
+				stderr_scanner.Buffer(stderr_scanner_buffer, maxCapacity)
+				stderr_scanner.Split(bufio.ScanLines)
+				go func() {
+					for stderr_scanner.Scan() {
+						errors_found = true
+						text := string(stderr_scanner.Text())
+						temp_error := fmt.Errorf(text)
+						stderr_array = append(stderr_array, fmt.Errorf(text))
+						if stderr_callback != nil {
+							go (*stderr_callback)(temp_error)
+						}
+					}
+					wg_stderr.Done()
+				}()
+			}
 
 			command_start_err := cmd.Start()
 			if command_start_err != nil {
@@ -88,11 +90,17 @@ func NewBashCommand() *BashCommand {
 				errors = append(errors, command_wait_err)
 			}
 
-			wg_stdout.Wait()
-			if errors_found {
-				wg_stderr.Wait()
-				errors = append(errors, stderr_array...)
+			if stdout_callback != nil {
+				wg_stdout.Wait()
 			}
+
+			if stderr_callback != nil {
+				if errors_found {
+					wg_stderr.Wait()
+					errors = append(errors, stderr_array...)
+				}
+			}
+			
 
 			if len(errors) > 0 {
 				return &stdout_array, errors
